0c7aa5f186397abb3b62164e806ee2eb841f99bd
TheBig-C <soymateovera@gmail.com>
2024-02-23 00:11:30 -0400

    ultimo

---
+++ src/Conexion.java
@@ -0,0 +1,13 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+	static final String DB_URl = "jdbc:postgresql://localhost/tasklist";
+    static final String USER ="postgres";
+    static final String PASS = "1234";
+
+    public static Connection obtenerConexion() throws SQLException {
+        return DriverManager.getConnection(DB_URl, USER, PASS);
+    }
+}
